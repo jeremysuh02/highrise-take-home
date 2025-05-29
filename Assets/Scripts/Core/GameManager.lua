27ac2ee8ff50c596db20c91c7cf488f8290fb665
jeremysuh02 <jsuh7227@gmail.com>
2025-05-29 00:52:51 -0500

    gamemanager client awake, gridmanager, new character variant

--- Assets/Scripts/Core/GameManager.lua
+++ Assets/Scripts/Core/GameManager.lua
@@ -1,2 +1,16 @@
 --!Type(Module)
 SaveManager = require("SaveManager")
+
+type PlayerData = SaveManager.PlayerData
+
+-- Event declarations
+SaveDataLoadedEvent = Event.new("SaveDataLoadedEvent")
+
+local function OnPlayerDataLoaded(playerData: PlayerData)
+    SaveDataLoadedEvent:Fire(playerData)
+    print("Player data loaded successfully for: " .. playerData.PlayerId)
+end
+
+function self:ClientAwake()
+    SaveManager.LoadPlayerDataFromServer(OnPlayerDataLoaded)
+end
